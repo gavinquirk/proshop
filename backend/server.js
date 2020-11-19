b27e2564758cb8abb3fea0c151dd9534992f8881
Gavin Quirk <gavinquirk@gmail.com>
2020-11-19 15:59:42 -0800

    react build, serve static files in production

--- backend/server.js
+++ backend/server.js
@@ -26,10 +26,6 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 
 // Routes
-app.get('/', (req, res) => {
-  res.send('API is running');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/orders', orderRoutes);
@@ -42,6 +38,19 @@ app.get('/api/config/paypal', (req, res) =>
 // Make uploads folder static
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+
+// Use react build in production
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running');
+  });
+}
 
 app.use(notFound);
 
---
+++ frontend/build/precache-manifest.ec1eaf7cd40c47fa5de41e599f7edd37.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3439494f1b8fed87c1d12c6f4a5d8b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1907346b3ba2fefe75af",
+    "url": "/static/css/main.81c1d096.chunk.css"
+  },
+  {
+    "revision": "542d4b382bcf1ffb2a0e",
+    "url": "/static/js/2.aaea2668.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.aaea2668.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1907346b3ba2fefe75af",
+    "url": "/static/js/main.128da7d3.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
